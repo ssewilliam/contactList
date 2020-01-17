cad123b98276809e2b5995be8c39a73b5ac714c1
ssewilliam <deriwilliams2008@gmail.com>
2020-01-17 19:03:32 +0300

    remove redux-devtools from logging

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
-import { composeWithDevTools } from "redux-devtools-extension";
 import rootSaga from "../middleware";
 import rootReducers from "../reducers";
 
 const sagaMiddleware = createSagaMiddleware();
-const middleware = composeWithDevTools(applyMiddleware(sagaMiddleware));
+const middleware = applyMiddleware(sagaMiddleware);
 
 const store = createStore(rootReducers, middleware);
 
